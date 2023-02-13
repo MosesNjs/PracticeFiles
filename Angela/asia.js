e3adc20815bcc0791f92235e1b73d0ce9d7c3a57
Moses Sopuluchukwu Njoku <mosessnjoku@gmail.com>
2023-02-13 01:10:34 +0100

    coming through

---
+++ Angela/asia.js
@@ -0,0 +1,17 @@
+// take notes here after consuming the content 
+// practice code here as well
+
+// assume two variables equal two different numbers, write code to switch the value of the variables 
+
+var a = 4;
+var b = 7; 
+
+// make a = 7 and b = 4
+
+var c = a;
+a = b; 
+b = c;
+
+console.log("a is " + a);
+console.log("b is " + b);
+

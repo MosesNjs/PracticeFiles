b386d73efddabd05b7847af51ed5f1560f2c7f28
Moses Sopuluchukwu Njoku <mosessnjoku@gmail.com>
2023-02-18 17:49:24 +0100

    commitment

--- promisesnj/index2.js
+++ promisesnj/index2.js
@@ -1,4 +1,5 @@
-/* fetch api here to replace everything
+/* 
+fetch api here to replace everything 
 
 times are achanging, we dont have to use xmlhttps ...
 we use the fecth api now, it has the promise api built in too
@@ -10,20 +11,43 @@ you gotta fetch it manually using
 response.json which returns a promise.
 you need to take the data the promise comes with using .then
 
+the if statement was just me playing around
 
-// the if statement was just me playing around
-
+async await, here to make it even easier
+async functions ALWAYS returns a promise. 
+putting all your asyncronous code in one function 
+the function is async so its non blocking 
+you can chain as many promises as you need 
 */
 
-fetch('/asyncbyninja/todos/mario.json').then((response)=>{
-    // if (response.status === 200){
-    console.log('resolved', response)
-    return response.json() 
-    // } else {
-    //     console.log('there was a client side issue')
-    // }
-}).then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log('rejected', err)
-})
+// fetch 
+// fetch('/asyncbyninja/todos/mario.json').then((response)=>{
+//     // if (response.status === 200){
+//     console.log('resolved', response)
+//     return response.json() 
+//     // } else {
+//     //     console.log('there was a client side issue')
+//     // }
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log('rejected', err)
+// })
+
+
+// async await 
+
+const getTodos = async () => { 
+
+    const response = await fetch('/asyncbyninja/todos/mario.json')
+
+    if (response.status !== 200){
+        throw new Error('cannot fetch the data')
+    }
+    const data = await response.json()
+    return data
+}
+
+getTodos()
+    .then(data => {console.log('promise resolved:', data)})
+    .catch(err => {console.log('promise rejected:', err.message)})
